5f3e451b05baf94b51091feae84ed530d9fb427a
John-Quinones <egrpanama@gmail.com>
2023-01-15 18:45:03 -0500

    Commit M07 JS Medio

--- M07 JavaScript Clases/Ejercicio 02.js
+++ M07 JavaScript Clases/Ejercicio 02.js
@@ -2,17 +2,33 @@
 /*2️⃣ EJERCICIO 02 2️⃣*/
 
 class Persona {
+   constructor(nombre, apellido, edad, domicilio) {
    // Crea el constructor de la clase "Persona".
    // Debe tener las propiedades: "nombre", "apellido", "edad" y "domicilio".
    // Debe tener un método llamado "detalle" que nos devuelva un objeto con las propiedades de la persona y
    // sus valores.
+      this.nombre = nombre,
+      this.apellido = apellido,
+      this.edad = edad,
+      this.domicilio = domicilio
+      this.detalle = function() {
+         return {
+            Nombre: this.nombre,
+            Apellido: this.apellido,
+            Edad: this.edad,
+            Domicilio: this.domicilio
+         }
+      }
+   }
 }
 
-function crearInstanciaPersona(nombre, apellido, edad, domicilio) {
+function crearInstanciaPersona(nombre, apellido, edad, dir) {
    // En este ejercicio debes crear una instancia de la clase construida en el ejercicio anterior.
    // Recibirás las propiedades por parámetro.
    // Retornar la instancia creada.
    // Tu código:
+   const persona = new Persona(nombre, apellido, edad, dir);
+   return persona;
 }
 
 function agregarMetodo() {
@@ -20,6 +36,9 @@ function agregarMetodo() {
    // Este método toma la propiedad "nombre" y "edad", y devuelve el string:
    // Ejemplo: "Juan, 22 años".
    // Tu código:
+   Persona.prototype.datos = function() {
+      return this.nombre + ", " + this.edad + " años";
+   }
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
